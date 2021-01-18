16d2636b03a62ddbfe6cb98e2121c372bc9f05ff
StasyTyapkina <stasya.tyapkina@gmail.com>
2021-01-18 13:32:46 +0300

    init: start task

---
+++ rsclone/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'app'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                loose: true,
+              }],
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
+    ],
+  },
+
+};
