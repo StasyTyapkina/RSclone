4c69f097143efa1eb576796527f1f2d0bf16ef86
StasyTyapkina <stasya.tyapkina@gmail.com>
2021-02-01 15:30:14 +0300

    feat: add music controller and setup modal

--- rsclone/src/modal.js
+++ rsclone/src/modal.js
@@ -1,20 +1,35 @@
-export const modal = document.querySelector('.modal_container');
-export const btn = document.querySelector('.modal_bttn');
-export const close = document.querySelector('.close_bttn');
+/* eslint-disable func-names */
+export const modalHelpContainer = document.querySelector('.help_container');
+export const modalSetupContainer = document.querySelector('.setup_container');
+export const helpOpenBttn = document.querySelector('.help_bttn');
+export const setupOpenBttn = document.querySelector('.setup_button');
+export const helpCloseBttn = document.querySelector('.help_container_close_bttn');
+export const setupCloseBttn = document.querySelector('.setup_container_close_bttn');
 
-btn.onclick = function () {
-  modal.style.display = 'block';
+helpOpenBttn.onclick = function () {
+  modalHelpContainer.style.display = 'block';
   document.querySelector('.video').play();
 };
 
-close.onclick = function () {
-  modal.style.display = 'none';
+helpCloseBttn.onclick = function () {
+  modalHelpContainer.style.display = 'none';
   document.querySelector('.video').pause();
 };
 
+setupOpenBttn.onclick = function () {
+  modalSetupContainer.style.display = 'block';
+};
+
+setupCloseBttn.onclick = function () {
+  modalSetupContainer.style.display = 'none';
+};
+
 window.onclick = function (e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
+  if (e.target === modalHelpContainer) {
+    modalHelpContainer.style.display = 'none';
     document.querySelector('.video').pause();
+  }
+  if (e.target === modalSetupContainer) {
+    modalSetupContainer.style.display = 'none';
   }
 };
--- rsclone/src/sound.js
+++ rsclone/src/sound.js
@@ -1,29 +1,47 @@
-export const buttonSound = document.querySelector('.sound_bttn');
+const buttonSound = document.querySelector('.sound_bttn');
+const musicControl = document.querySelector('.music_control_slider');
+const musicValue = document.querySelector('.music_value');
+const soundsControl = document.querySelector('.sound_control_slider');
+const soundsValue = document.querySelector('.sounds_value');
 
 buttonSound.sound = false;
 buttonSound.classList.add('sound_bttn--activatable');
 buttonSound.classList.toggle('sound_bttn--active', buttonSound.sound);
 
-export function toggleSound() {
+const audioMusic = document.createElement('audio');
+audioMusic.id = 'audio_music';
+audioMusic.src = './src/sounds/main.mp3';
+audioMusic.loop = true;
+audioMusic.volume = 0.4;
+document.body.appendChild(audioMusic);
+
+const audioSound = document.createElement('audio');
+audioSound.id = 'audio_sound';
+audioSound.src = '';
+audioSound.volume = 0.4;
+document.body.appendChild(audioSound);
+
+function toggleSound() {
   buttonSound.sound = !buttonSound.sound;
 }
-
-export const audio = document.createElement('audio');
-audio.id = 'audio';
-audio.src = './src/sounds/main.mp3';
-audio.loop = true
-
-document.body.appendChild(audio);
 
 buttonSound.addEventListener('click', () => {
   toggleSound();
   buttonSound.classList.toggle('sound_bttn--active', buttonSound.sound);
 
   if (buttonSound.sound) {
-    audio.play();
+    audioMusic.play();
     buttonSound.innerHTML = '<i class="fas fa-volume-up"></i>';
   } else {
-    audio.pause();
+    audioMusic.pause();
     buttonSound.innerHTML = '<i class="fas fa-volume-off">';
   }
+});
+
+document.addEventListener('input', () => {
+  audioMusic.volume = musicControl.value;
+  musicValue.innerHTML = `${Math.round(audioMusic.volume * 100)}  %`;
+
+  audioSound.volume = soundsControl.value;
+  soundsValue.innerHTML = `${Math.round(audioSound.volume * 100)}  %`;
 });
